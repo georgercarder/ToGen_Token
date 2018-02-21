30742688b8caef8f52d627e6f4f9b970c322676c
George Carder <georgercarder@gmail.com>
2018-02-21 11:00:40 -0800

    finished basic templte

--- outline.sol
+++ outline.sol
@@ -95,6 +95,17 @@ contract ToGen {
 
   //destroys tokens from account
   //burnFrom
+	function burnFrom(address _from, uint256 _value) 
+	  public 
+		returns (bool success){
+		require(balanceOf[_from] >= _value);
+		require(_value <= allowance[_from][msg.sender]);
+		balanceOf[_from] -= _value;
+	  allowance[_from][msg.sender] -= _value;
+		totalSupply -= _value;
+		Burn(_from, _value);
+		return true;	
+	}
 }
 
 
